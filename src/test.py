2be0e46c488a09b6ac9c8195ad9db40233d4aecd
romzzes <Romanpopov120793@gmail.com>
2019-10-15 18:13:08 +0300

    Test

---
+++ src/test.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome("C:/Users/r.popov/Desktop/python course/chromedriver.exe")
+
+driver.get("https://www.wikipedia.org/")
+
+search_field = driver.find_element_by_id("searchInput")
+search__button = driver.find_element_by_xpath("//form[@id = 'search-form']/fieldset/button")
+
+search_field.send_keys("Test Automation")
+search__button.click()
+
+assert "Test automation" in driver.title
+
+driver.quit()
